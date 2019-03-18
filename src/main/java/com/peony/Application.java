2fa08159a1649bef28644c4da0cd6b8aa372f06a
BlackHe <1124681011@qq.com>
2019-03-18 18:04:00 +0800

    字符/字节概念梳理，字节中存储的是ascii码值 1字节=8位 -->  1byte=8bit,即一个asicc码值可以转换为一个8位的二进制数

--- src/main/java/com/peony/Application.java
+++ src/main/java/com/peony/Application.java
@@ -1,5 +1,8 @@
 package com.peony;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 public class Application {
 
 
@@ -7,8 +10,15 @@ public class Application {
     protected final void testMethod(){
         System.out.println("testMethod");
     }
-    public static void main(String[] args) {
-        Application application = null;
-        application.testMethod();
+    //utf-8下，一个中文字符占3个字节
+    //GB2312下，一个中文字符占2个字节
+    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+        ArrayList<Object> list = new ArrayList<>();
+        byte[] bytes_utf8 = new String("何文先").getBytes("utf-8");
+        byte[] bytes_gb2312 = new String("何文先").getBytes("gb2312");
+        System.out.println("UTF-8编码，一个中文字符占["+bytes_utf8.length/3+"]个字节");
+        System.out.println("GB2312编码，一个中文字符占["+bytes_gb2312.length/3+"]个字节");
+
+
     }
 }
